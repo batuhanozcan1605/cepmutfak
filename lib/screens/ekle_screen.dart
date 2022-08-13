ca6f93f4ab6db6a54c6f9527dc9e88699894b4da
Batuhan\batuhanozcan1605 <batuhanozcan1605@gmail.com>
2022-08-13 13:23:25 +0300

    willpopscope çalışıyor. yeni UrunCard sadece yazıldı

--- lib/screens/ekle_screen.dart
+++ lib/screens/ekle_screen.dart
@@ -39,6 +39,10 @@ class _EkleScreenState extends State<EkleScreen> {
     await Urunlerdao().updateContent(urun_id, 1);
   }
 
+  Future<void>  urunMutfaktaDegil(urun_id) async {
+    await Urunlerdao().updateContent(urun_id, 0);
+  }
+
   /*Future<void> kategoriGoster() async {
     var liste = await Kategorilerdao().allKategoriler();
 
@@ -58,192 +62,201 @@ class _EkleScreenState extends State<EkleScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Color(0xFF4D818C),
-        title: SvgPicture.asset('images/appbar_logo.svg'),
-        centerTitle: true,
-      ),
-      body: Column(
-        children: [
-          Container(
-            decoration: const BoxDecoration(
-              color: Colors.white,
-              boxShadow: [
-                BoxShadow(
-                  color: const Color(0x1a000000),
-                  offset: Offset(0, 3),
-                  blurRadius: 6,
+    return WillPopScope(
+      onWillPop: () async {
+        if(isSearching) {
+          return false;
+        } else {
+          return true;
+        }
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Color(0xFF4D818C),
+          title: SvgPicture.asset('images/appbar_logo.svg'),
+          centerTitle: true,
+        ),
+        body: Column(
+          children: [
+            Container(
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                    color: const Color(0x1a000000),
+                    offset: Offset(0, 3),
+                    blurRadius: 6,
+                  ),
+                ],
+              ),
+              child: Padding(
+                padding: const EdgeInsets.fromLTRB(20, 0, 20, 6),
+                child: TextField(
+                  decoration: InputDecoration(
+                    fillColor: Color(0xFF4D818C),
+                    prefixIconColor: Color(0xFF4D818C),
+                    prefixIcon: const Icon(Icons.search),
+                    suffix: isSearching ? IconButton(
+                      icon: const Icon(Icons.cancel),
+                      onPressed: (){
+                        setState(() {
+                          isSearching = false;
+                        });
+                      },
+                    ) : Center(),
+                    hintText: 'Ürün ekle',
+                  ),
+                  onTap: (){
+                    setState(() {
+                      isSearching = true;
+                    });
+                  },
+                  onChanged: (result){
+                    setState(() {
+                      query = result;
+                      print(result);
+                    });
+                  },
                 ),
-              ],
-            ),
-            child: Padding(
-              padding: const EdgeInsets.fromLTRB(20, 0, 20, 6),
-              child: TextField(
-                decoration: InputDecoration(
-                  fillColor: Color(0xFF4D818C),
-                  prefixIconColor: Color(0xFF4D818C),
-                  prefixIcon: const Icon(Icons.search),
-                  suffix: isSearching ? IconButton(
-                    icon: const Icon(Icons.cancel),
-                    onPressed: (){
-                      setState(() {
-                        isSearching = false;
-                      });
-                    },
-                  ) : Center(),
-                  hintText: 'Ürün ekle',
-                ),
-                onTap: (){
-                  setState(() {
-                    isSearching = true;
-                  });
-                },
-                onChanged: (result){
-                  setState(() {
-                    query = result;
-                    print(result);
-                  });
-                },
               ),
             ),
-          ),
-           isSearching ? Center() : Padding(
-            padding: const EdgeInsets.only(top: 20, left: 20, right: 20),
-            child: SizedBox(
-              height: 80,
-              child: Card(
-                clipBehavior: Clip.antiAlias,
-                shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10),
-                ),
-                child: Stack(
-                  children: [
-                    Align(
-                      alignment: Alignment.bottomLeft,
-                        child: SvgPicture.asset('images/icon_ekle.svg')),
-                    Align(
-                      alignment: Alignment.center,
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: const [
-                          Text('Kendine Özel Ürün Ekle',
-                          style: TextStyle(
-                            fontFamily: 'Segoe UI',
-                            fontSize: 15,
-                            color: Color(0xff013440),
-                            fontWeight: FontWeight.w600,
-                          )
-                            ),
-                          Text('(Öncelikle arama kısmından kontrol edin.)',
-                              style: TextStyle(
-                                fontFamily: 'Segoe UI',
-                                fontSize: 10,
-                                color: Color(0xFF4D818C),
-                              )
-                          )
-                        ],
-                      ),
-                    )
-                  ]
-                ),
+             isSearching ? Center() : Padding(
+              padding: const EdgeInsets.only(top: 20, left: 20, right: 20),
+              child: SizedBox(
+                height: 80,
+                child: Card(
+                  clipBehavior: Clip.antiAlias,
+                  shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Stack(
+                    children: [
+                      Align(
+                        alignment: Alignment.bottomLeft,
+                          child: SvgPicture.asset('images/icon_ekle.svg')),
+                      Align(
+                        alignment: Alignment.center,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: const [
+                            Text('Kendine Özel Ürün Ekle',
+                            style: TextStyle(
+                              fontFamily: 'Segoe UI',
+                              fontSize: 15,
+                              color: Color(0xff013440),
+                              fontWeight: FontWeight.w600,
+                            )
+                              ),
+                            Text('(Öncelikle arama kısmından kontrol edin.)',
+                                style: TextStyle(
+                                  fontFamily: 'Segoe UI',
+                                  fontSize: 10,
+                                  color: Color(0xFF4D818C),
+                                )
+                            )
+                          ],
+                        ),
+                      )
+                    ]
+                  ),
+              ),
             ),
-          ),
-          ),
-          isSearching ?
-              Expanded(
-                  child: FutureBuilder<List<Urunler>>(
-                    future: searchUrunler(query),
-                    builder: (context, snapshot) {
-                      if(snapshot.hasData) {
-                        var urunlerList = snapshot.data;
-                        return ListView.builder(
-                            itemCount: urunlerList!.length,
-                            itemBuilder: (context, index) {
-                              var urun = urunlerList[index];
-                                return Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: Container(
-                                      child:
-                                      Row(
-                                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                          children: [
-                                            Padding(
-                                              padding: const EdgeInsets.only(left:  10.0),
-                                              child: Text(
-                                                urun.urun_name,
-                                                style: TextStyle(
-                                                  fontWeight: FontWeight.w600,
-                                              ),
-                                              ),
-                                            ),
-                                            Padding(
-                                              padding: const EdgeInsets.only(right: 10.0),
-                                              child: IconButton(
-                                                onPressed: (){
-                                                  urunMutfakta(urun.urun_id);
-                                                  final snackBar = SnackBar(
-                                                      action: SnackBarAction(
-                                                          label: 'Geri Al',
-                                                          onPressed: (){
-
-                                                      }),
-                                                      content: Text("${urun.urun_name} Mutfağa Eklendi",
-                                                      style: TextStyle(
-                                                        color: Colors.white70,
-                                                       ),
-                                                      ),
-                                                      backgroundColor: Color(0xFF013440),
-                                                  );
-                                                  ScaffoldMessenger.of(context).showSnackBar(snackBar);
-                                                },
-                                                  icon: Icon(Icons.add_circle, color: Color(0xFF4D818C),),
-                                              ),
-                                            ),
-                                          ])
-                                  ),
-                                );
-                            }
-                        );
-                      } else {
-                        return Center();
-                      }
-                    },
-                  )
-              )
-              : Expanded(
-                  child: FutureBuilder<List<Kategoriler>>(
-                      future: showAllKategoriler(),
+            ),
+            isSearching ?
+                Expanded(
+                    child: FutureBuilder<List<Urunler>>(
+                      future: searchUrunler(query),
                       builder: (context, snapshot) {
-                        if (snapshot.hasData) {
-                          var kategoriList = snapshot.data;
-                          return GridView.builder(
-                              padding: const EdgeInsets.all(20),
-                              gridDelegate:
-                                  const SliverGridDelegateWithFixedCrossAxisCount(
-                                      crossAxisCount: 2,
-                                      childAspectRatio: 5 / 2),
-                              itemCount: kategoriList!.length,
+                        if(snapshot.hasData) {
+                          var urunlerList = snapshot.data;
+                          return ListView.builder(
+                              itemCount: urunlerList!.length,
                               itemBuilder: (context, index) {
-                                var kategori = kategoriList[index];
-                                return GestureDetector(
-                                  onTap: () {
-                                    Navigator.push(
-                                        context,
-                                        MaterialPageRoute(
-                                            builder: (context) => UrunlerBodyScreen(
-                                                  kategori: kategori,
-                                                )));
-                                  },
-                                  child: kategoriCard(kategori.kategori_name, kategori.kategori_image),
-                                );
-                              });
+                                var urun = urunlerList[index];
+                                  return Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Container(
+                                        child:
+                                        Row(
+                                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                            children: [
+                                              Padding(
+                                                padding: const EdgeInsets.only(left:  10.0),
+                                                child: Text(
+                                                  urun.urun_name,
+                                                  style: TextStyle(
+                                                    fontWeight: FontWeight.w600,
+                                                ),
+                                                ),
+                                              ),
+                                              Padding(
+                                                padding: const EdgeInsets.only(right: 10.0),
+                                                child: IconButton(
+                                                  onPressed: (){
+                                                    urunMutfakta(urun.urun_id);
+                                                    final snackBar = SnackBar(
+                                                        action: SnackBarAction(
+                                                            label: 'Geri Al',
+                                                            onPressed: (){
+                                                              urunMutfaktaDegil(urun.urun_id);
+                                                        }),
+                                                        content: Text("${urun.urun_name} Mutfağa Eklendi",
+                                                        style: TextStyle(
+                                                          color: Colors.white70,
+                                                         ),
+                                                        ),
+                                                        backgroundColor: Color(0xFF013440),
+                                                    );
+                                                    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                                                  },
+                                                    icon: Icon(Icons.add_circle, color: Color(0xFF4D818C),),
+                                                ),
+                                              ),
+                                            ])
+                                    ),
+                                  );
+                              }
+                          );
                         } else {
-                          return const Center(child: Text(""));
+                          return Center();
                         }
-                      }),
-                ),
-        ],
+                      },
+                    )
+                )
+                : Expanded(
+                    child: FutureBuilder<List<Kategoriler>>(
+                        future: showAllKategoriler(),
+                        builder: (context, snapshot) {
+                          if (snapshot.hasData) {
+                            var kategoriList = snapshot.data;
+                            return GridView.builder(
+                                padding: const EdgeInsets.all(20),
+                                gridDelegate:
+                                    const SliverGridDelegateWithFixedCrossAxisCount(
+                                        crossAxisCount: 2,
+                                        childAspectRatio: 5 / 2),
+                                itemCount: kategoriList!.length,
+                                itemBuilder: (context, index) {
+                                  var kategori = kategoriList[index];
+                                  return GestureDetector(
+                                    onTap: () {
+                                      Navigator.push(
+                                          context,
+                                          MaterialPageRoute(
+                                              builder: (context) => UrunlerBodyScreen(
+                                                    kategori: kategori,
+                                                  )));
+                                    },
+                                    child: kategoriCard(kategori.kategori_name, kategori.kategori_image),
+                                  );
+                                });
+                          } else {
+                            return const Center(child: Text(""));
+                          }
+                        }),
+                  ),
+          ],
+        ),
       ),
     );
   }
---
+++ lib/widgets/urun_card.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+class UrunCard extends StatelessWidget {
+
+  final String urun_name;
+  final String urun_image;
+  final onTap;
+  final bool tapped;
+
+  UrunCard(this.urun_name, this.urun_image, this.onTap, this.tapped);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Card(
+        clipBehavior: Clip.antiAlias,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Column(
+          children: [
+            Flexible(
+              child: Image.asset('images/${urun_image}'),
+              flex: 3,
+            ),
+            Flexible(
+              flex: 1,
+              child: Container(
+                height: 25,
+                color: tapped ? Colors.tealAccent : Colors.white,
+                child: Padding(
+                  padding: const EdgeInsets.only(bottom: 4),
+                  child: Align(
+                    alignment: Alignment.bottomCenter,
+                    child: FittedBox(
+                      fit: BoxFit.fitHeight,
+                      child: Text(
+                        urun_name,
+                        style: const TextStyle(
+                          fontFamily: 'Segoe UI',
+                          fontSize: 12,
+                          color: const Color(0xff013440),
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
